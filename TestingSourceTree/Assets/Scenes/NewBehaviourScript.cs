5f3c392fd3985c42de90cbd042428331cde5a57e
eeshik <ee_shi_kiat@yahoo.com.sg>
2021-08-28 22:08:25 +0800

    chages

--- TestingSourceTree/Assets/Scenes/NewBehaviourScript.cs
+++ TestingSourceTree/Assets/Scenes/NewBehaviourScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public GameObject rule;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(rule.name);
     }
 
     // Update is called once per frame
